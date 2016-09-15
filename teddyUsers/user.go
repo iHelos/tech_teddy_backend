b4cf6bbc98af2f6ac19f0ccbbe6e7f859507deb2
iHelos <ihelos.ermakov@gmail.com>
2016-09-15 19:30:49 +0300

    login/register/logout completed

--- teddyUsers/user.go
+++ teddyUsers/user.go
@@ -5,13 +5,13 @@ import (
 	"github.com/asaskevich/govalidator"
 	"encoding/json"
 	"errors"
-	"golang.org/x/crypto/bcrypt"
-//	"os/user"
+	//	"os/user"
 )
 
 type UserStorageEngine interface {
 	Create(string, string, string) (error)
 	Load(string) (NewUser, error)
+	CheckLogin(string, string) (error)
 }
 
 type UserStorage struct {
@@ -20,65 +20,84 @@ type UserStorage struct {
 }
 
 type NewUser struct {
-	Name        string `json:"name" valid:"required" form:"name"`
-	Email string `json:"email" valid:"required,email" form:"email"`
-	Password string `json:"password" valid:"required" form:"password"`
+	Name          string `json:"name" valid:"required" form:"name"`
+	Email         string `json:"email" valid:"required,email" form:"email"`
+	Password      string `json:"password" valid:"required" form:"password"`
 	CheckPassword string `json:"password2" valid:"required" form:"password2"`
 }
 
-
-func (storage *UserStorage) CreateUser(ctx *iris.Context) (*NewUser, error){
+func (storage *UserStorage) CreateUser(ctx *iris.Context) (error) {
 	ctx.Request.Body()
 	var user = NewUser{}
-	err := ctx.ReadForm(&user)
-	if err !=nil{
-		return nil, err
+	var err error
+	if string(ctx.Request.Header.ContentType()) == "application/json" {
+		err = json.Unmarshal(ctx.Request.Body(), &user)
+	} else {
+		err = ctx.ReadForm(&user)
+	}
+	if err != nil {
+		return err
 	}
 	err = user.check()
-	if err != nil{
-		return nil, err
+	if err != nil {
+		return err
 	}
-	hashpassword,err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
-	err = storage.Engine.Create(user.Name, user.Email, string(hashpassword))
-	if err != nil{
-		return nil, err
+	err = storage.Engine.Create(user.Name, user.Email, user.Password)
+	if err != nil {
+		return err
 	}
 	ctx.Session().Set("name", user.Name)
-	return &user, nil
+	return nil
 }
 
-func (storage *UserStorage) CreateUserApi(ctx *iris.Context) (*NewUser, error){
+type LoginUser struct {
+	Name     string `json:"name" valid:"required" form:"name"`
+	Password string `json:"password" valid:"required" form:"password"`
+}
+
+func (storage *UserStorage) LoginUser(ctx *iris.Context) (error) {
 	ctx.Request.Body()
-	var user NewUser
-	err := json.Unmarshal(ctx.Request.Body(), &user)
-	if err !=nil{
-		return nil, err
+	var user LoginUser
+	var err error
+	if string(ctx.Request.Header.ContentType()) == "application/json" {
+		err = json.Unmarshal(ctx.Request.Body(), &user)
+	} else {
+		err = ctx.ReadForm(&user)
+	}
+	if err != nil {
+		return err
 	}
 
 	err = user.check()
-	if err != nil{
-		return nil, err
+	if err != nil {
+		return err
 	}
 
-	hashpassword,err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
-	err = storage.Engine.Create(user.Name, user.Email, string(hashpassword))
-	if err != nil{
-		return nil, err
+	err = storage.Engine.CheckLogin(user.Name, user.Password)
+	if err != nil {
+		return err
 	}
 	ctx.Session().Set("name", user.Name)
-	return &user, nil
+	return nil
 }
 
 func (user *NewUser) check() error {
-	if check, err := govalidator.ValidateStruct(user); !check{
+	if check, err := govalidator.ValidateStruct(user); !check {
 		return err
-	} else if user.Password != user.CheckPassword{
+	} else if user.Password != user.CheckPassword {
 		return errors.New("passwords mismatch")
 	}
 	return nil
 }
 
-func New(cookiepath string) *UserStorage{
+func (user *LoginUser) check() error {
+	if check, err := govalidator.ValidateStruct(user); !check {
+		return err
+	}
+	return nil
+}
+
+func New(cookiepath string) *UserStorage {
 	config := Config{
 		SessionCookieName: cookiepath,
 	}
