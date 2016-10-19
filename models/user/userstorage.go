a0287022ff50732b7b3ae516e04748fe58d71b5c
iHelos <ihelos.ermakov@gmail.com>
2016-10-19 12:37:20 +0300

    new tokens struct

--- models/user/userstorage.go
+++ models/user/userstorage.go
@@ -11,6 +11,7 @@ import (
 	"github.com/NaySoftware/go-fcm"
 	"fmt"
 	"github.com/labstack/gommon/log"
+	"github.com/dgrijalva/jwt-go"
 )
 
 type UserStorageEngine interface {
@@ -28,27 +29,24 @@ type UserStorage struct {
 var fcmList cmap.ConcurrentMap
 
 const (
+	hmacUserSecret = "95CCEB5921E59B285AC773E4963E1"
 	serverKey = "AIzaSyACAJnKTfYG9_gmBna5TU-VA57ssTl3TVk"
 )
 
-func init() {
-	fcmList = cmap.New()
-}
-
-func (storage *UserStorage) CreateUser(ctx *iris.Context) (error) {
-	ctx.Request.Body()
+func (storage *UserStorage) CreateUser(ctx *iris.Context) (string,string,error) {
+	log.Print(ctx.Request.Header.String())
 	var user = NewUser{}
 	var err error
 	err = json.Unmarshal(ctx.Request.Body(), &user)
 	if err != nil {
 		UserError := NewUserError()
 		UserError.Append("request", 0)
-		return UserError
+		return "","",UserError
 	}
 	user.Login = strings.ToLower(user.Login)
 	err = user.check()
 	if err != nil {
-		return err
+		return "","",err
 	}
 	err = storage.Engine.Create(user.Login, user.Email, user.Password1)
 	if err != nil {
@@ -56,15 +54,25 @@ func (storage *UserStorage) CreateUser(ctx *iris.Context) (error) {
 		if trnerror, ok := err.(tarantool.Error); ok {
 			if trnerror.Code == 32771 {
 				UserError.Append("login", 1)
-				return UserError
+				return "","", UserError
 			}
 		}
 		UserError.Append("DB", 0)
-		return UserError
+		return "","", UserError
 	}
-	ctx.Session().Set("name", user.Login)
-	fcmList.Set(user.FCMToken,"")
-	return nil
+	//ctx.Session().Set("name", user.Login)
+	userToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"login": user.Login,
+		"type":"user",
+	})
+	bearToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"login": user.Login,
+		"type":"bear",
+	})
+	userTokenSigned, err := userToken.SignedString([]byte(hmacUserSecret))
+	bearTokenSigned, err := bearToken.SignedString([]byte(hmacUserSecret))
+
+	return userTokenSigned,bearTokenSigned,nil
 }
 
 type LoginUser struct {
@@ -73,26 +81,34 @@ type LoginUser struct {
 	FCMToken string `json:"fcm"`
 }
 
-func (storage *UserStorage) LoginUser(ctx *iris.Context) (error) {
-	ctx.Request.Body()
+func (storage *UserStorage) LoginUser(ctx *iris.Context) (string,string,error) {
 	var user LoginUser
 	var err error
 	err = json.Unmarshal(ctx.Request.Body(), &user)
 	if err != nil {
 		UserError := NewUserError()
 		UserError.Append("request", 0)
-		return UserError
+		return "","", UserError
 	}
 	user.Login = strings.ToLower(user.Login)
 	err = storage.Engine.CheckLogin(user.Login, user.Password)
 	if err != nil {
 		UserError := NewUserError()
 		UserError.Append("request", 1)
-		return UserError
+		return "","",UserError
 	}
-	fcmList.Set(user.FCMToken, "")
-	ctx.Session().Set("name", user.Login)
-	return nil
+	userToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"login": user.Login,
+		"type":"user",
+	})
+	bearToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"login": user.Login,
+		"type":"bear",
+	})
+	userTokenSigned, err := userToken.SignedString([]byte(hmacUserSecret))
+	bearTokenSigned, err := bearToken.SignedString([]byte(hmacUserSecret))
+
+	return userTokenSigned,bearTokenSigned,nil
 }
 
 func New(cookiepath string) *UserStorage {
@@ -103,8 +119,28 @@ func New(cookiepath string) *UserStorage {
 }
 
 func (storage *UserStorage) MustBeLogged(ctx *iris.Context) {
-	sid := ctx.GetCookie(storage.Config.SessionCookieName)
-	err := storage.Engine.CheckIsLogged(sid)
+
+	signedtoken := ctx.Request.Header.Peek("Authorization")
+	token, err := jwt.Parse(string(signedtoken), func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(hmacUserSecret), nil
+	})
+	if (err!=nil){
+		ctx.JSON(iris.StatusOK, REST.GetResponse(
+			1,
+			map[string]int{
+				"loginstatus":0,
+			},
+		))
+		return
+	}
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		fmt.Println(claims["login"])
+	} else {
+		fmt.Println(err)
+	}
 	if err != nil {
 		ctx.JSON(iris.StatusOK, REST.GetResponse(
 			1,
@@ -117,7 +153,7 @@ func (storage *UserStorage) MustBeLogged(ctx *iris.Context) {
 	}
 }
 
-func SendAll(){
+func SendAll() {
 	data := map[string]string{
 		"msg": "Что за странный медведь",
 		"sum": "ГОВОРИ!",
