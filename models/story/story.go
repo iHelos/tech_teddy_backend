c2f4af0fdf4d91610a244b1fd09dddbbba02422e
iHelos <ihelos.ermakov@gmail.com>
2016-10-19 11:19:47 +0300

    story struct changed

--- models/story/story.go
+++ models/story/story.go
@@ -2,15 +2,17 @@ package story
 
 //import "encoding/json"
 
-type Story struct  {
-	Name string `json:"name"`
+type Story struct {
+	Name        string `json:"name"`
 	Description string `json:"description"`
-	Author string `json:"author"`
-	ID string `json:"id"`
-	Price int `json:"price"`
+	Author      string `json:"author"`
+	Minutes     int `json:"minutes"`
+	Seconds     int `json:"seconds"`
+	ID          string `json:"id"`
+	Price       int `json:"price"`
 }
 
-func GetAllStories() []Story{
+func GetAllStories() []Story {
 	story1 := Story{Name:"Story1", Description:"Story1 awesome description", Author:"iHelos", ID:"1", Price:15}
 	story2 := Story{Name:"Story2", Description:"Story2 awesome description", Author:"AnnJelly", ID:"2", Price:25}
 	//json1, _ := json.Marshal(story1)
@@ -18,7 +20,7 @@ func GetAllStories() []Story{
 	return []Story{story1, story2}
 }
 
-func GetMyStories() []Story{
+func GetMyStories() []Story {
 	story1 := Story{Name:"Story1", Description:"Story1 awesome description", Author:"iHelos", ID:"1", Price:15}
 	//json1, _ := json.Marshal(story1)
 	//json2, _ := json.Marshal(story2)
