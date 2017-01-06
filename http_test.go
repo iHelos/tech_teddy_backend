1613a1c9ed7900d36b6674712d285ee6a5d522e7
ihelos <ihelos.ermakov@gmail.com>
2017-01-07 02:11:37 +0300

    Fix: example stories

--- http_test.go
+++ http_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 	"net/http"
 	."github.com/iHelos/tech_teddy/model"
+	"fmt"
 )
 
 func BenchmarkHttpParallel(b *testing.B) {
@@ -30,7 +31,7 @@ func ExampleReverse() {
 		Name:"баба яга",
 		Category:1,
 		Price:0,
-		Duration:"5:55",
+		Duration:"05:55",
 		Description:"Жили-были муж с женой, и была у них дочка. Заболела жена и умерла. Погоревал-погоревал мужик да и женился на другой.Невзлюбила злая баба девочку, била ее, ругала, только и думала, как бы совсем извести, погубить.",
 		AuthorID:1,
 		Roled:false,
@@ -55,4 +56,70 @@ func ExampleReverse() {
 		},
 	}
 	UpdateStory(story)
+	fmt.Println(story.Name)
+	// Output:
+	// баба яга
+}
+
+func ExampleStory() {
+	story := Story{
+		ID:14,
+		Name:"солнце и ветер",
+		Category:4,
+		Price:0,
+		Duration:"02:05",
+		Description:"сказка о том, как солнце и ветер поспорили",
+		AuthorID:1,
+		Roled:true,
+		DurationSplitted:Duration{
+			Minutes:2,
+			Seconds:5,
+		},
+		ImgUrls:UrlImage{
+			Small:"http://storage.googleapis.com/hardteddy_images/small/14.jpg",
+			Large:"http://storage.googleapis.com/hardteddy_images/large/14.jpg",
+		},
+		Parts:[]StoryPart{
+			{
+				Part:"Часть 1",
+				Text:"Однажды сердитый северный Ветер и Солнце затеяли спор о том, кто из них сильнее. Долго они спорили и решили испробовать свою силу на одном путешественнике. Ветер сказал: -Я сейчас вмиг сорву с него плащ!",
+				Audio:UrlAudio{
+					Raw:"http://storage.googleapis.com/hardteddy_stories/14_1.raw",
+					Original:"http://storage.googleapis.com/hardteddy_stories/mp3/14_1.mp3",
+				},
+				Size:148570,
+			},
+			{
+				Part:"Часть 2",
+				Text:"И начал дуть. Он дул очень сильно и долго. Но человек только плотнее закутывался в свой плащ.",
+				Audio:UrlAudio{
+					Raw:"http://storage.googleapis.com/hardteddy_stories/14_2.raw",
+					Original:"http://storage.googleapis.com/hardteddy_stories/mp3/14_2.mp3",
+				},
+				Size:72860,
+			},
+			{
+				Part:"Часть 3",
+				Text:"Тогда Солнце начало пригревать путника. Он сначала опустил воротник, потом развязал пояс, а потом снял плащ и понёс его на руке.",
+				Audio:UrlAudio{
+					Raw:"http://storage.googleapis.com/hardteddy_stories/14_3.raw",
+					Original:"http://storage.googleapis.com/hardteddy_stories/mp3/14_3.mp3",
+				},
+				Size:115510,
+			},
+			{
+				Part:"Часть 4",
+				Text:"Солнце сказало Ветру: - Видишь: добром, лаской, можно добиться гораздо большего, чем насилием.",
+				Audio:UrlAudio{
+					Raw:"http://storage.googleapis.com/hardteddy_stories/14_4.raw",
+					Original:"http://storage.googleapis.com/hardteddy_stories/mp3/14_4.mp3",
+				},
+				Size:85510,
+			},
+		},
+	}
+	UpdateStory(story)
+	fmt.Println(story.Name)
+	// Output:
+	// баба яга
 }
